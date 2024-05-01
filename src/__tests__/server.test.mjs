6596aec6563810276ad8bd6c98d4da6640aba681
arty <arthur.buldauskas@itbit.com>
2024-05-01 16:49:31 -0400

    more coverage

--- src/__tests__/server.test.mjs
+++ src/__tests__/server.test.mjs
@@ -101,6 +101,16 @@ test.serial('cors support', async (t) => {
   // OPTIONS should 204 automatically
   let response = await fetch(`http://jambox.test.com/foobar`, opts);
   t.like(response, { status: 204, statusText: 'No Content' });
+
+  response = await fetch(`http://jambox.test.com/foobar`, {
+    ...opts,
+    method: 'GET',
+  });
+
+  t.like(Object.fromEntries(response.headers.entries()), {
+    // This is set by jambox automatically when cors is enabled
+    'access-control-allow-origin': '*',
+  });
 });
 
 test.serial('auto mocks', async (t) => {
